52b76071b6522e184566d54a633b96097183d6a0
jiachingjian <sagiters1128@gmail.com>
2022-04-17 20:49:16 +0800

    Init

---
+++ Assets/Scenes/HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityCI
+{
+    public class HelloWorld : MonoBehaviour
+    {
+        private int _test = 1;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log(_test);
+        }
+
+    }
+
+}
